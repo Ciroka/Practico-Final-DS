fa293e5fe7d0a5405f5d3dc324a2527b7c061dc9
Juan Cruz Olguín <olguinjuancruz@gmail.com>
2026-06-28 01:29:13 +0000

    fix(back): agregar Auth + Admin a products.controller.ts

--- back/src/products/controllers/products.controller.ts
+++ back/src/products/controllers/products.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query, UseGuards } from '@nestjs/common';
 
-import { JwtAuthGuard } from '../../shared/guards';
+import { Roles } from '../../shared/decorators/roles.decorator';
+import { UserRole } from '../../shared/enums';
+import { JwtAuthGuard, RolesGuard } from '../../shared/guards';
 import { PaginatedResult } from '../../shared/pagination/pagination.type';
 import { QueryParamsProductDto, CreateProductDto, UpdateProductDto, UpdateStock, ProductResponse } from '../dto';
 import { ProductsService } from '../services/products.service';
@@ -16,27 +18,36 @@ export class ProductsController {
   async findAll(@Query() params: QueryParamsProductDto): Promise<PaginatedResult<ProductResponse>> {
     return this.productsService.findAll(params);
   }
-
+  
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   async findOne(@Param('id') id: string): Promise<ProductResponse> {
     return this.productsService.findOne(Number(id));
   }
-
+  
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
   @Post()
   async create(@Body() body: CreateProductDto): Promise<ProductResponse> {
     return this.productsService.create(body);
   }
-
+  
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
   @Put(':id')
   async update(@Param('id') id: string, @Body() body: UpdateProductDto): Promise<ProductResponse> {
     return this.productsService.update(Number(id), body);
   }
-
+  
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
   @Patch('/:id/stock')
   async updateStock(@Param('id') id: string, @Body() body: UpdateStock): Promise<ProductResponse> {
     return this.productsService.updateStock(Number(id), body);
   }
-
+  
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
   @Delete(':id')
   async remove(@Param('id') id: string): Promise<ProductResponse> {
     return this.productsService.remove(Number(id));
